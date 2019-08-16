9546aed3d5e38ab7e5870b0c073d365bfd705321
uzushino <torattoria.mamiya@gmail.com>
2019-08-16 20:32:08 +0900

    Add http server.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 
 use clap::{Arg, crate_version};
 use std::io::Read;
+use actix_web::{web, App, HttpResponse, HttpServer};
+use std::sync::mpsc;
+use std::thread;
+use futures::future::Future;
 
 fn main() -> Result<(), failure::Error> {
     env_logger::init();
@@ -19,6 +23,20 @@ fn main() -> Result<(), failure::Error> {
                 .help("Sets a show markdown time interval(second).")
                 .takes_value(true)
         )
+        .arg(
+            Arg::with_name("SERVER")
+                .long("server")
+                .short("s")
+                .help("Running http server.")
+        )
+        .arg(
+            Arg::with_name("PORT")
+                .long("port")
+                .short("p")
+                .default_value_if("SERVER", None, "8080")
+                .takes_value(true)
+                .help("Sets a http server port.")
+        )
         .get_matches();
 
     let interval = matches
@@ -36,6 +54,31 @@ fn main() -> Result<(), failure::Error> {
     };
 
     run(input.as_str(), interval)?;
+
+    Ok(())
+}
+
+fn run_with_server(input: &str, interval: u64) -> Result<(), failure::Error> {
+    let app = violet::App::new(
+        input.to_string(),
+        interval,
+    );
+
+    thread::spawn(move || {
+        let sys = actix_rt::System::new("http-server");
+
+        let _ = HttpServer::new(|| {
+            App::new().route("/", web::get().to(|| {
+                HttpResponse::Ok()
+            }))
+        })
+        .bind("127.0.0.1:8088")
+        .unwrap()
+        .start();
+        let _ = sys.run();
+    });
+    
+    app.run();
 
     Ok(())
 }

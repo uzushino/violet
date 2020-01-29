1ee7f940b7c747d70349605423afc1991dd46976
uzushino <torattoria.mamiya@gmail.com>
2020-01-29 23:10:24 +0900

    impl js type to string

--- src/builtin/mod.rs
+++ src/builtin/mod.rs
@@ -39,6 +39,15 @@ pub fn value_to_vector(value: &ValueData) -> anyhow::Result<Vec<String>> {
                     .map(|data| {
                         match data.deref().borrow() {
                             ValueData::String(s) => s.to_string(),
+                            ValueData::Number(n) => n.to_string(),
+                            ValueData::Null => "<NULL>".to_string(),
+                            ValueData::Boolean(b) => {
+                                if *b {
+                                    "TRUE".to_string()
+                                } else {
+                                    "FALSE".to_string()
+                                }
+                            },
                             _ => String::default(),
                         }
                     })

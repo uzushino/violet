a7c196000bf7acf04a800e6abbf0bb5c93aa0114
uzushino <torattoria.mamiya@gmail.com>
2020-01-30 22:17:55 +0900

    Fix table from object.

--- src/builtin/core.rs
+++ src/builtin/core.rs
@@ -1,9 +1,14 @@
 use std::io::Read;
-
+use std::{
+    ops::{Deref, DerefMut},
+};
+use linked_hash_map::LinkedHashMap as HashMap;
+use std::borrow::Borrow;
 use boa::{
     builtins::{
         function::NativeFunctionData,
         value::{to_value, ResultValue, Value, ValueData},
+        object::{InternalState, InternalStateCell, Object, ObjectKind, INSTANCE_PROTOTYPE, PROTOTYPE},
     },
     exec::Interpreter,
 };
@@ -12,6 +17,8 @@ use crate::{
     make_builtin_fn, 
     builtin::value_to_vector 
 };
+
+use serde_json::{map::Map, Number as JSONNumber, Value as JSONValue};
 
 pub fn stdin(_this: &Value, _args: &[Value], _: &mut Interpreter) -> ResultValue {
     let mut buf = String::default();
@@ -22,44 +29,59 @@ pub fn stdin(_this: &Value, _args: &[Value], _: &mut Interpreter) -> ResultValue
     }
 }
 
-pub fn table(_this: &Value, args: &[Value], _: &mut Interpreter) -> anyhow::Result<Value> {
-    let args = args.get(0).ok_or(ValueData::Null)
-        .map_err(|_| anyhow::Error::msg("Could not get 1st argument."))?;
+fn value_to_map(obj: &gc::GcCell<Object>) -> HashMap<String, String> {
+    let mut new_obj = HashMap::new();
 
-    let mut rows = Vec::new();
-    let ary = value_to_vector(args);
-
-    /*
-    for m in b.0.iter() {
-        let mut row = linked_hash_map::LinkedHashMap::new();
-        for (k, v) in m.0.clone() {
-            let s = match v {
-                JsValue::String(s) => s.to_string(),
-                JsValue::Int(i) => i.to_string(),
-                JsValue::Float(f) => f.to_string(),
-                _ => "".to_string(),
+    for (k, v) in obj.borrow().internal_slots.iter() {
+        if k != INSTANCE_PROTOTYPE {
+            let s = match v.deref().borrow() {
+                ValueData::String(s) => s.to_string(),
+                ValueData::Number(n) => n.to_string(),
+                ValueData::Null => "<NULL>".to_string(),
+                ValueData::Boolean(b) => {
+                    if *b {
+                        "TRUE".to_string()
+                    } else {
+                        "FALSE".to_string()
+                    }
+                },
+                _ => String::default(),
             };
-            
-            row.insert(k.to_string(), s);
+
+            new_obj.insert(k.clone(), s);
         }
-        rows.push(row);
     }
-    */
+
+    new_obj
+}
+
+pub fn table(_this: &Value, args: &[Value], _: &mut Interpreter) -> ResultValue {
+    let args = args.get(0).ok_or(ValueData::Null)
+        .map_err(|_| anyhow::Error::msg("Could not get 1st argument.")).unwrap();
+
+    let mut arr = Vec::default();
+    match args.deref().borrow() {
+        &ValueData::Object(ref obj) => {
+            let new_obj = value_to_map(obj);
+            arr.push(new_obj)
+        },
+        _ => {}
+    }
 
     let opt = madato::types::RenderOptions {
         headings: None,
         ..Default::default()
     };
-
-    let table = madato::mk_table(rows.as_slice(), &Some(opt));
     
+    let _table = madato::mk_table(arr.as_slice(), &Some(opt));
+
     Ok(gc::Gc::new(ValueData::Null))
 }
 
 pub fn create_constructor(global: &Value) -> Value {
     let module = ValueData::new_obj(Some(global));
 
-    //make_builtin_fn!(table, named "table", with length 1, of module);
+    make_builtin_fn!(table, named "table", with length 1, of module);
     make_builtin_fn!(stdin, named "stdin", of module);
  
    module 

0e369cffd64dce91e2e27166f71d19b40ed83ad6
uzushino <torattoria.mamiya@gmail.com>
2020-03-09 23:23:44 +0900

    Remove input

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,4 @@
-use chrono::prelude::*;
-use std::sync::{
-    mpsc::{self, Receiver, Sender},
-    Arc, Mutex,
-};
-use std::thread::{self, JoinHandle};
-use std::time;
 mod builtin;
-mod cursor;
-mod event;
-pub mod input;
 pub mod markdown;
 mod script;
 

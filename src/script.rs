7aebe3dd28bef30d2b9bd91d0c6afd6f120ee614
uzushino <torattoria.mamiya@gmail.com>
2021-02-16 22:13:56 +0900

    script eval

--- src/script.rs
+++ src/script.rs
@@ -13,15 +13,9 @@ impl Isolate {
 
     pub fn eval<A: Into<String>>(&self, script: A) -> Result<String, gc::Gc<boa::builtins::value::ValueData>> {
         let mut context = Context::new();
+       
         context.register_global_class::<builtin::core::Violet>();
-        let global = &realm.global_object;
-
-        global.("Violet", builtin::core::create_constructor(global));
-        global.insert("Mysql", builtin::mysql::create_constructor(global));
-
-        let engine = &mut Executor::new(realm);
         
-        boa::forward_val(engine, script.into().as_str())
-            .map(|s| s.to_string())
+        context.eval(script)
     }
 }

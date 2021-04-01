f0e3aa47b0f49e42d823c539a764356613fb8570
uzushino <torattoria.mamiya@gmail.com>
2021-04-01 20:07:48 +0900

    impl table

--- src/builtin/core.rs
+++ src/builtin/core.rs
@@ -1,8 +1,8 @@
-use std::collections::HashMap;
+use std::{collections::HashMap};
 use boa::{
+    property::PropertyDescriptor,
     class::{Class, ClassBuilder},
     gc::{Finalize, Trace},
-    property::Attribute,
     Context, Result, Value,
 };
 
@@ -19,12 +19,21 @@ impl Class for Violet {
     }
 
     fn init(class: &mut ClassBuilder) -> Result<()> {
-        class.static_method("table", 1, |_this, args, _ctx| {
+        class.static_method("table", 1, |_this, args, ctx| {
             if let Some(arg) = args.get(0) {
                 if let Some(object) = arg.as_object() {
-                    if object.is_map() {
-                        return Ok(true.into()); // return `true`.
+                    let mut table: HashMap<String, String> = HashMap::new();
+
+                    for (key, value) in object.borrow().string_properties().next() {
+                        match value { 
+                            PropertyDescriptor::Data(desc) => {
+                                table.insert(key.to_string(), desc.value().to_string(ctx)?.to_string());
+                            },
+                            _ => {}
+                        }
                     }
+
+                    return Ok(true.into()); // return `true`.
                 }
             }
 
